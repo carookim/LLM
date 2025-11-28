1a610bfed450e7181f614cf6d133e751f7c5d74e
carookim <carookim77@gmail.com>
2025-11-28 09:29:27 +0900

    20251128

--- LLM3/rag_4.0.py
+++ LLM3/rag_4.0.py
@@ -10,3 +10,14 @@ if not api_key:
 
 # 필수 라이브러리 로드
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_chroma import Chroma
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.documents import Document
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import time
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+docs_path = os.path.join(script_dir,'sample_docs')
